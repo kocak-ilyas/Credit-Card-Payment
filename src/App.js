9869a5294da18bf2ef5eaecfc43b7540e17db4bf
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-01-24 17:40:49 +0300

    it works

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+// import "./style.css"
 
-export default function App() {
-  const [card, setCard] = useState({
-    cvc: "",
-    expiry: "",
-    focus: "",
-    name: "",
-    number: "",
-  });
+function App() {
+  // const [card, setCard] = useState(initialState)
+  const [number, setNumber] = useState("");
+  const [name, setName] = useState("");
+  const [expiry, setExpiry] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [focus, setFocus] = useState("");
 
-  const handleInputFocus = (e) => {
-    setCard({ focus: e.target.name });
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCard({ [name]: value });
-  };
   return (
-    <div id="PaymentForm">
+    <div id="App">
       <Cards
-        cvc={card.cvc}
-        expiry={card.expiry}
-        focused={card.focus}
-        name={card.name}
-        number={card.number}
+        number={number}
+        name={name}
+        expiry={expiry}
+        cvc={cvc}
+        focused={focus}
       />
       <form>
         <input
           type="tel"
           name="number"
           placeholder="Card Number"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
         />
-        ...
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="text"
+          name="expiry"
+          placeholder="MM/YY Expiry"
+          value={expiry}
+          onChange={(e) => setExpiry(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="tel"
+          name="cvc"
+          placeholder="CVC"
+          value={cvc}
+          onChange={(e) => setCvc(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
       </form>
     </div>
   );
 }
+export default App;
